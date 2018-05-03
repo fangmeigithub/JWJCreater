339e52f79c5ed54c24c4edda526c711eeffbfcab
liubaojian <3095167957@qq.com>
2018-05-03 15:03:53 +0800

    第三次提交

---
+++ JWJFramework/MyNewView.h
@@ -0,0 +1,13 @@
+//
+//  MyNewView.h
+//  JWJFramework
+//
+//  Created by liubaojian on 2018/5/3.
+//  Copyright © 2018年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyNewView : UIView
+
+@end
---
+++ JWJFramework/MyNewView.m
@@ -0,0 +1,15 @@
+//
+//  MyNewView.m
+//  JWJFramework
+//
+//  Created by liubaojian on 2018/5/3.
+//  Copyright © 2018年 admin. All rights reserved.
+//
+
+#import "MyNewView.h"
+
+@implementation MyNewView
+
+//这是一个测试的View
+
+@end
